243677b43d8c7d09a33f463b3e7213092548c502
songtao <songtao@kicen.com>
2019-01-14 13:39:17 +0800

    0.3.4

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ install_requires = [
 ]
 
 setup(name='wings-sanic',
-      version='0.3.3',
+      version='0.3.4',
       description='The wings-sanic is a lightweight python framework aimed at making it as simple as possible to document your Sanic API with Swagger UI, Plus param validation and model serialization.',
       long_description='The wings-sanic is a lightweight python framework aimed at making it as simple as possible to document your Sanic API with Swagger UI, Plus param validation and model serialization.',
       author='SongTao',
