134dcb415ed7a7aca1cbdd2d0d0f7909d2d0b973
songtao <songtao@kicen.com>
2019-03-11 10:50:28 +0800

    0.5.16 content-type debug

--- wings_sanic/views.py
+++ wings_sanic/views.py
@@ -124,7 +124,7 @@ async def extract_params(request, metadata):
 
     # body
     if metadata.body_serializer:
-        if 'application/json' in utils.get_value(request.headers, 'content-type', []):
+        if 'application/json' in utils.get_value(request.headers, 'content-type', ['application/json']):
             body_data = request.json
         else:
             body_data = from_form_data({
